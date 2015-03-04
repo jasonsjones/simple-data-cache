44f025808ac12ae35d75b918742ed5c5afe57c0a
Jason Jones <jsjones96@gmail.com>
2015-03-04 04:48:45 +0000

    add some unit tests

--- test/dictionary.spec.js
+++ test/dictionary.spec.js
@@ -27,4 +27,33 @@ describe('Dictionary Unit Tests', function() {
         dict.isEmpty().should.equal(true);
         dict.size().should.equal(0);
     });
+
+    it('should add items to the dictionary', function () {
+        dict.set('dict', 'data structure');
+        dict.set('set', 'another data structure');
+        dict.size().should.equal(2);
+    });
+
+    it('should determine if the dictionary has a particular key', function () {
+        dict.set('dict', 'data structure');
+        dict.set('set', 'another data structure');
+        dict.has('dict').should.equal(true);
+        dict.has('set').should.equal(true);
+    });
+
+    it('should correctly remove an item', function () {
+        dict.set('dict', 'data structure');
+        dict.set('set', 'another data structure');
+        dict.remove('dict').should.equal(true);
+        dict.size().should.equal(1);
+        dict.remove('nothing').should.equal(false);
+    });
+
+    it('should correctly get an item', function () {
+        dict.set('dict', 'data structure');
+        dict.set('set', 'another data structure');
+        dict.get('dict').should.equal('data structure');
+        dict.get('set').should.equal('another data structure');
+        dict.get('nothing').should.equal(-1);
+    });
 });
