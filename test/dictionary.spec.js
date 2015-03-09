53a576de8bc1d5cdfc74e8bec042bf7064017405
Jason Jones <jsjones96@gmail.com>
2015-03-09 21:02:30 +0000

    add additional unit tests

--- test/dictionary.spec.js
+++ test/dictionary.spec.js
@@ -56,4 +56,44 @@ describe('Dictionary Unit Tests', function() {
         dict.get('set').should.equal('another data structure');
         dict.get('nothing').should.equal(-1);
     });
+
+    it('should return all the keys in the dictionary', function () {
+        dict.set('dict', 'data structure');
+        dict.set('set', 'another data structure');
+        dict.set('linked-list', 'a very good data structure');
+        var keys = dict.keys();
+        keys.should.be.an.Array;
+        keys.should.be.length(3);
+        keys.should.containEql('dict', 'set', 'linked-list');
+    });
+
+    it('should return all the values in the dictionary', function () {
+        dict.set('dict', 'data structure');
+        dict.set('set', 'another data structure');
+        dict.set('linked-list', 'a very good data structure');
+        var values = dict.values();
+        values.should.be.an.Array;
+        values.should.be.length(3);
+        values.should.containEql('data structure',
+            'another data structure', 'a very good data structure');
+    });
+
+    it('should return the complete contents of the dictionary', function () {
+        dict.set('dict', 'data structure');
+        dict.set('set', 'another data structure');
+        dict.set('linked-list', 'a very good data structure');
+        var items = dict.getItems();
+        items.should.be.an.Object;
+        items.should.have.properties('set', 'dict', 'linked-list');
+    });
+
+    it('should clear the dictionary of all items', function () {
+        dict.set('dict', 'data structure');
+        dict.set('set', 'another data structure');
+        dict.set('linked-list', 'a very good data structure');
+        dict.size().should.equal(3);
+        dict.clear();
+        dict.size().should.equal(0);
+        dict.isEmpty().should.equal(true);
+    });
 });
