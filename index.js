042b6e4c34420318aa5b19f9b59a399ad8d96b92
Jason S. Jones <jsjones96@gmail.com>
2015-03-15 07:59:10 -0700

    add jsdoc comments to source

--- index.js
+++ index.js
@@ -37,20 +37,41 @@
         /**
          * Determines if the dictionary is empty
          *
-         * @ returns {boolean} true if dictionary is empty, false otherwise
+         * @returns {boolean} true if dictionary is empty, false otherwise
          */
         isEmpty: function () {
             return this.size() === 0;
         },
 
+        /**
+         * Adds the key/value pair to the data store
+         *
+         * @ params {string} key the key to store in the data store
+         * @ params {number|string|object} value the value to associate with
+         *          the key in the data store
+         */
         set: function (key, value) {
             this._items[key] = value;
         },
 
+        /**
+         * Determines if the data store contains the key
+         *
+         * @params {string} key the key to find in the data store
+         * @returns {boolean} true if the key is in the data store, false
+         *          otherwise
+         */
         has: function (key) {
             return key in this._items;
         },
 
+        /**
+         * Removes the item with the matching key from the data store
+         *
+         * @params {string} key the key of the item to remove from the data store
+         * @returns {boolean} true if the item with the key wsa removed, false
+         *          otherwise
+         */
         remove: function (key) {
             if (this.has(key)) {
                 delete this._items[key];
@@ -59,6 +80,14 @@
             return false;
         },
 
+        /**
+         * Returns the item with the matching key from the data store
+         *
+         * @params {string} key the key of the item to return from the data store
+         * @returns {string|number|object} the value of the item from the data
+                    store with the matching key if it is contained in the data
+                    store.  If it is not in the data store, return -1.
+         */
         get: function (key) {
             if (this.has(key)) {
                 return this._items[key];
@@ -67,10 +96,22 @@
             }
         },
 
+        /**
+         * Returns an array of all the keys stored in the data store
+         *
+         * @returns {object} an array containing all the keys stored in the
+                    data store
+         */
         keys: function () {
             return Object.keys(this._items);
         },
 
+        /**
+         * Returns an array of all the values stored in the data store
+         *
+         * @returns {object} an array containing all the values stored in the
+                    data store
+         */
         values: function () {
             var values = [];
             for (var key in this._items) {
@@ -81,15 +122,24 @@
             return values;
         },
 
+        /**
+         * Returns all the key/value pairs stored in the data store
+         *
+         * @returns {object} all key/value pairs stored in the data store
+         */
         getItems: function () {
             return this._items;
         },
 
+        /**
+         * Clears the data store of all key/value pairs
+         */
         clear: function () {
             this._items = {};
         }
     };
 
+    // Expose SimpleDataStore
     module.exports = SimpleDataStore;
 
 }());
